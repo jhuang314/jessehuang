7f30580ed669c4ef6374d02c2a37e8dde47efa2b
Jesse Huang <jh3141@gmail.com>
2014-03-05 05:00:42 -0500

    added keep alive

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var http = require('http');
 
 app.get('/', function(req, res) {
     res.send("I'm Jesse Huang");
@@ -8,3 +9,27 @@ var port = Number(process.env.PORT || 5000);
 app.listen(port, function () {
     console.log("listening on " + port);
 });
+
+function startKeepAlive() {
+    setInterval(function() {
+        var options = {
+            host: 'jessehuang.herokuapp.com',
+            port: 80,
+            path: '/'
+        };
+        http.get(options, function(res) {
+            res.on('data', function(chunk) {
+                try {
+                    // optional logging... disable after it's working
+                    console.log("HEROKU RESPONSE: " + chunk);
+                } catch (err) {
+                    console.log(err.message);
+                }
+            });
+        }).on('error', function(err) {
+            console.log("Error: " + err.message);
+        });
+    }, 20 * 60 * 1000); // load every 20 minutes
+}
+
+startKeepAlive();
