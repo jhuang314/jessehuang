9ed9a911a910f6dbc8409ff967c12479a3a736a9
Jesse Huang <jh3141@gmail.com>
2014-03-06 02:54:29 -0500

    added content

--- app.js
+++ app.js
@@ -1,43 +1,70 @@
+// Modules
 var express = require('express');
-var app = express();
 var http = require('http');
+var app = express();
+//var routes = require('./routes');
+var path = require('path');
 
+// Configuration
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.static(__dirname + '/public'));
+
+var env = process.env.NODE_ENV || 'development';
+
+// development only
+if ('development' == env) {
+    app.locals.pretty=true;
+}
+
+
+// Routes
 app.get('/', function(req, res) {
-    res.send("I'm Jesse Huang");
-});
-var port = Number(process.env.PORT || 5000);
-app.listen(port, function () {
-    console.log("listening on " + port);
+    res.render('index.jade', {
+	title: "Jesse Huang",
+	pageTitle: "Jesse Huang"    
+    });
 });
 
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+
+
+
+// Content unrelated stuff goes here:
 var NA = require("nodealytics");
 NA.initialize('UA-48713710-1', 'jessehuang.com', function () {
-  NA.trackPage('utmdt=Jesse%20Huang', 'pageName', function (err, resp) {
-    if (!err, resp.statusCode === 200) {
-      console.log('Analytics Worked!');
-    }
-  });
+    NA.trackPage('utmdt=Jesse%20Huang', 'pageName', function (err, resp) {
+	if (!err, resp.statusCode === 200) {
+	    console.log('Analytics Worked!');
+	}
+    });
 });
 
 
 function startKeepAlive() {
     setInterval(function() {
         var options = {
-            host: 'jessehuang.herokuapp.com',
-            port: 80,
-            path: '/'
+	    host: 'jessehuang.herokuapp.com',
+	    port: 80,
+	    path: '/'
         };
         http.get(options, function(res) {
-            res.on('data', function(chunk) {
+	    res.on('data', function(chunk) {
                 try {
-                    // optional logging... disable after it's working
-                    console.log("HEROKU RESPONSE: " + chunk);
+		    // optional logging... disable after it's working
+		    console.log("HEROKU RESPONSE: " + chunk);
                 } catch (err) {
-                    console.log(err.message);
+		    console.log(err.message);
                 }
-            });
+	    });
         }).on('error', function(err) {
-            console.log("Error: " + err.message);
+	    console.log("Error: " + err.message);
         });
     }, 20 * 60 * 1000); // load every 20 minutes
 }
