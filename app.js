a73774c603dae9f18b3a9553e9a55133fe67f31f
Jesse Huang <jh3141@gmail.com>
2014-03-09 23:52:21 -0400

    added blog features

--- app.js
+++ app.js
@@ -9,13 +9,20 @@ var express = require('express')
 
 // get Poet up and running
 var poet = Poet(app, {
-    posts: './_posts/',
-    postsPerPage: 5,
-    metaFormat: 'json',
-    routes: {
-	'/post/:post' : 'post'
-    }
+    posts: './_posts/'
+//    postsPerPage: 5,
+//    metaFormat: 'json'
 });
+
+poet.watch(function () {
+    // watcher reloaded
+}).init().then(function () {
+    poet.clearCache();
+    Object.keys(poet.posts).map(function (title) {
+	var post = poet.posts[title];
+    });
+});
+
 
 // Configuration
 app.set('port', process.env.PORT || 3000);
@@ -31,9 +38,8 @@ if ('development' == env || true) {
     app.locals.pretty=true;
 }
 
-poet.init().then(function () {
-    
-});
+
+
 
 // Routes
 app.get('/', function(req, res) {
@@ -46,7 +52,56 @@ app.get('/', function(req, res) {
     });
 });
 
+app.get('/blog', function(req, res) {
+    res.render('blog');
+});
 
+poet.addRoute('/blog/mypost/:post', function (req, res) {
+    var post = poet.helpers.getPost(req.params.post);
+    if (post) {
+	res.render('post', { post: post }); 
+    } else {
+	res.send(404);
+    }
+});
+
+poet.addRoute('/blog/mytags/:tag', function (req, res) {
+    var taggedPosts = poet.helpers.postsWithTag(req.params.tag);
+    if (taggedPosts.length) {
+	res.render('tag', {
+	    posts: taggedPosts,
+	    tag: req.params.tag
+	});
+    }
+});
+
+poet.addRoute('/blog/mycategories/:category', function (req, res) {
+    var categorizedPosts = poet.helpers.postsWithCategory(req.params.category);
+    if (categorizedPosts.length) {
+	res.render('category', {
+	    posts: categorizedPosts,
+	    category: req.params.category
+	});
+    }
+});
+
+poet.addRoute('/blog/mypages/:page', function (req, res) {
+    var page = req.params.page,
+    lastPost = page * 3;
+    res.render('page', {
+	posts: poet.helpers.getPosts(lastPost - 3, lastPost),
+	page: page
+    });
+});
+
+
+app.get('/sitemap.xml', function (req, res) {
+  // Only get the latest posts
+  var postCount = poet.helpers.getPostCount();
+  var posts = poet.helpers.getPosts(0, postCount);
+  res.setHeader('Content-Type', 'application/xml');
+  res.render('sitemap', { posts: posts });
+});
 
 var server = http.createServer(app).listen(app.get('port'), function(){
     console.log('Express server listening on port ' + app.get('port'));
